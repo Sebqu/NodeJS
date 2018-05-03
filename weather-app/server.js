da7212243cd41afdbd7d350acf0b0802b1ceda9f
Sebastian G <sebastian.gorzelnik@gmail.com>
2018-05-03 20:20:24 +0200

    Problem with partials - can name only if name is connected with html syntax (background ect)

--- weather-app/server.js
+++ weather-app/server.js
@@ -1,19 +1,38 @@
 const express = require('express');
 const hbs = require('hbs');
+const geolocation = require('./geolocation')
+const weather = require('./weather')
+
 
 const port = process.env.PORT || 3000;
 var app = express();
 
+    
+hbs.registerPartials(__dirname + '/views/partials');
 app.set('view engive', 'hbs');
+
 app.get('/', (req, res) =>{
-    res.render('home.hbs');
+    res.render('home.hbs', {
+        err: undefined
+    })
       
 });  
 app.get('/weather', (req, res) => { 
     var city = req.query.city; //mytext is the name of your input box
-    res.send('Your Text:' +city); 
-    console.log(city);
-}); 
+    geolocation.geoLocation (city, (err, result) =>{
+        if(err){
+            res.render('home.hbs', {
+                err: `${err}.`
+            })
+        }else{
+            res.render('weather.hbs',{
+                pageTitle: city,
+
+
+            })
+        }
+    });
+});
 
 
 
